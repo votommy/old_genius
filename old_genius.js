74aa101539a27a593bfc3e7c7692ab1a990d5e93
votommy <votommy@outlook.com>
2024-10-04 19:46:06 -0700

    Version 2
    This improvement applies the URL update before load, instead of after.

--- old_genius.js
+++ old_genius.js
@@ -1,1 +1,12 @@
-window.location.href += "?bagon=1";
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeNetRequest.updateDynamicRules({
+        addRules: [{
+           id: 1,
+           priority: 1,
+           action: { type: 'redirect', redirect: { transform: { queryTransform: { addOrReplaceParams: [{ key: 'bagon', value: '1' }] } } } },
+           condition: { urlFilter: 'https://genius.com/*-lyrics', resourceTypes: ['main_frame'] }
+        }],
+        removeRuleIds: [1]
+    });
+});
+  
