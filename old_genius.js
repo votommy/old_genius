f2557abd826f0dd3782724c5132232e5c4748011
votommy <votommy@outlook.com>
2024-10-04 19:36:19 -0700

    Undo breaking changes

--- old_genius.js
+++ old_genius.js
@@ -1,11 +1,1 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      if (!details.url.includes("?bagon=1")) {
-        // Modify the URL
-        return {redirectUrl: details.url + "?bagon=1"};
-      }
-    },
-    {urls: ["https://genius.com/*-lyrics"]},
-    ["blocking"]
-  );
-  
+window.location.href += "?bagon=1";
